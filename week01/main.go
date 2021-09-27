cd79366e2adf2b8fe6f49128310f26b68a1daa8a
oscartaracena <oscar.taracena@gmail.com>
2021-09-27 16:51:58 -0700

    adding and testing code

--- week01/main.go
+++ week01/main.go
@@ -9,4 +9,13 @@ func main() {
 	fmt.Println("Hello World")
 	//Printing todo
 	fmt.Println("Printing, TODO")
+	a := "Say \"hello\" to Go!"
+	fmt.Println(a)
+	fmt.Printf("%s\n", a)
+
+	b := "Hello, 世界"
+	for i, c := range b {
+		fmt.Printf("%d: %s\n", i, string(c))
+	}
+
 }
