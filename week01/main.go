c41d6b4d605d5ebe97878d6f4da3f5ed8b829872
oscartaracena <oscar.taracena@gmail.com>
2021-09-29 15:08:50 -0700

    adding homework code

--- week01/main.go
+++ week01/main.go
@@ -9,10 +9,23 @@ func main() {
 	fmt.Println("Hello World")
 	//Printing todo
 	fmt.Println("Printing, TODO")
+	
+	//printing String ("Go")
+	str := "Go"
+	fmt.Println("Printing", str)
+	
+	//printing int (42) 
+	numVal := 42
+	fmt.Println("Printing", numVal)
+	
+	//printing bool (true)
+	
+	fmt.Println("Printing")	
+
+	//ignore just testing that is all 
 	a := "Say \"hello\" to Go!"
 	fmt.Println(a)
 	fmt.Printf("%s\n", a)
-
 	b := "Hello, 世界"
 	for i, c := range b {
 		fmt.Printf("%d: %s\n", i, string(c))
