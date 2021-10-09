383fb2bdadcd605e543cb2cc56d9e41337f557a7
oscartaracena <oscar.taracena@gmail.com>
2021-10-09 13:22:23 -0700

    changes in code to slice
    slice test code

--- week02/assigment02_test.go
+++ week02/assigment02_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 )
 
+//array test code
 func TestArray(t *testing.T) {
 	exp := [4]string{"John", "Paul", "George", "Ringo"}
 	act := [4]string{}
@@ -19,4 +20,23 @@ func TestArray(t *testing.T) {
 		}
 	}
 
+}
+
+//testing function for Slice
+
+func TestSlice(t *testing.T) {
+
+	exp := []string{"five", "six", "seven", "eight"}
+	act := []string{"five", "six", "seven", "eight"}
+	fmt.Println("Testing ....")
+	for i, v := range exp {
+		act[i] = v
+		fmt.Printf("%d - %s\n", i, v)
+	}
+	for i, v := range act {
+		if act[i] != exp[i] {
+			t.Errorf("act[%d] : %s is not equal to exp[%d] : %s ", i, v, i, exp[i])
+		}
+	}
+	fmt.Println("End Testing")
 }
