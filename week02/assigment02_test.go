3fea5492bec62fcda1a07ea440ceb772e3f0a36c
oscartaracena <oscar.taracena@gmail.com>
2021-10-09 12:55:58 -0700

    fix error
    Error, missing '}'

--- week02/assigment02_test.go
+++ week02/assigment02_test.go
@@ -1,13 +1,15 @@
-package main 
+package main
 
 import (
 	"fmt"
 	"testing"
 )
+
 func TestArray(t *testing.T) {
 	exp := [4]string{"John", "Paul", "George", "Ringo"}
 	act := [4]string{}
-	
+
 	for i, v := range exp {
-	act[i] = v
+		act[i] = v
 	}
+}
